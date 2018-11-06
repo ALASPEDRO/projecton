61a07e5acdaa323e61389b7a7b74df29b3e1b140
ALASPEDRO <pmalver@gmail.com>
2018-11-06 18:06:36 +0100

    Create app.py
    Simple welcome

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import flaskapp = FLASK(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+if __name__ == "__main__"
+    app.run(host="0.0.0.0", port=8080)
